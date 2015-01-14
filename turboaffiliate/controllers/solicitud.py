2b23effca575aa228e27f6acd391761625f273ee
Carlos Flores <cafg10@gmail.com>
2015-01-13 21:09:44 -0600

    PEP 8

--- turboaffiliate/controllers/solicitud.py
+++ turboaffiliate/controllers/solicitud.py
@@ -19,13 +19,15 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-from turbogears import (controllers, flash, redirect, identity, expose,
-                        validate, validators)
-from turboaffiliate import model
 from decimal import Decimal
 
+from turbogears import (controllers, flash, redirect, identity, expose,
+                        validate, validators)
+
+from turboaffiliate import model
+
+
 class Solicitud(controllers.Controller):
-    
     @expose()
     @validate(validators=dict(affiliate=validators.Int(), periodo=validators.Int(),
                               entrega=validators.DateTimeConverter(format='%d/%m/%Y'),
@@ -33,41 +35,37 @@ class Solicitud(controllers.Controller):
                               monto=validators.String()))
     @identity.require(identity.not_anonymous())
     def agregar(self, affiliate, **kw):
-        
         kw['affiliate'] = model.Affiliate.get(affiliate)
         kw['monto'] = Decimal(kw['monto'].replace(',', ''))
         model.Solicitud(**kw)
-        
+
         flash('Agregada la solicitud')
-        
+
         raise redirect('/affiliate/{0}'.format(kw['affiliate'].id))
-    
+
     @expose()
     @validate(validators=dict(solicitud=validators.Int()))
     def convertir(self, solicitud):
-        
         solicitud = model.Solicitud.get(solicitud)
         prestamo = solicitud.prestamo(identity.current.user)
         solicitud.destroySelf()
-        
+
         raise redirect('/loan/{0}'.format(prestamo.id))
-    
+
     @expose()
     @identity.require(identity.not_anonymous())
     @validate(validators=dict(solicitud=validators.Int()))
     def eliminar(self, solicitud):
-        
         solicitud = model.Solicitud.get(solicitud)
         affiliate = solicitud.affiliate
         solicitud.destroySelf()
-        
+
         raise redirect('/affiliate/{0}'.format(affiliate.id))
-    
+
     @expose(template='turboaffiliate.templates.solicitud.dia')
     @identity.require(identity.not_anonymous())
     @validate(validators=dict(dia=validators.DateTimeConverter(format='%d/%m/%Y')))
     def dia(self, dia):
-        
         solicitudes = model.Solicitud.selectBy(entrega=dia)
-        
+
         return dict(solicitudes=solicitudes, dia=dia)
