7fad5e4768f2cf9e9fbdca9c229ce91d48fc4da2
Carlos Flores <cafg10@gmail.com>
2015-07-10 08:37:03 -0600

    PEP 8

--- turboaffiliate/controllers/billing.py
+++ turboaffiliate/controllers/billing.py
@@ -19,99 +19,102 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
+from datetime import date
+
 from turbogears import controllers, identity
 from turbogears import expose, validate, validators
-from turboaffiliate import model
-from datetime import date
 from sqlobject.sqlbuilder import AND
 
+from turboaffiliate import model
+
+
 class Billing(controllers.Controller):
-	
-	@identity.require(identity.not_anonymous())
-	@expose(template='turboaffiliate.templates.affiliate.billing.index')
-	def index(self):
-		
-		return dict()
-	
-	@identity.require(identity.not_anonymous())
-	@expose(template='turboaffiliate.templates.affiliate.billing.status')
-	@validate(validators=dict(departamento=validators.Int()))
-	def state(self, departamento):
-		
-		"""Muestra los estados de cuenta de aportaciones de los afiliados de un
-		Departamento
-		
-		:param departamento: El identificador del :class:`Departamento` a mostrar
-		"""
-		
-		departamento = model.Departamento.get(departamento)
-		affiliates = model.Affiliate.selectBy(departamento=departamento)
-		
-		return dict(affiliates=affiliates, day=date.today())
-	
-	@identity.require(identity.not_anonymous())
-	@expose(template='turboaffiliate.templates.affiliate.billing.status')
-	@validate(validators=dict(payment=validators.String()))
-	def payment(self, payment):
-		
-		affiliates = model.Affiliate.selectBy(payment=payment)
-		affiliates = (a for a in affiliates if a.joined != None)
-		
-		return dict(affiliates=affiliates, day=date.today())
-	
-	@identity.require(identity.not_anonymous())
-	@expose(template='turboaffiliate.templates.affiliate.billing.status')
-	@validate(validators=dict(school=validators.String()))
-	def school(self, school):
-		
-		affiliates = model.Affiliate.selectBy(school=school)
-		affiliates = (a for a in affiliates if a.joined != None)
-		
-		return dict(affiliates=affiliates, day=date.today())
-	
-	@identity.require(identity.not_anonymous())
-	@expose(template='turboaffiliate.templates.affiliate.billing.status')
-	@validate(validators=dict(start=validators.Int(), end=validators.Int))
-	def rango(self, start, end):
-		
-		affiliates = model.Affiliate.select(AND(model.Affiliate.q.id>=start,
-											model.Affiliate.q.id<=end))
-		affiliates = (a for a in affiliates if a.joined != None)
-		
-		return dict(affiliates=affiliates, day=date.today())
-	
-	@identity.require(identity.not_anonymous())
-	@expose(template='turboaffiliate.templates.affiliate.billing.loans')
-	@validate(validators=dict(departamento=validators.Int()))
-	def loanState(self, departamento):
-		
-		"""Muestra los estados de cuenta préstamos de los afiliados de un
-		departamento
-		
-		:param departamento: El identificador del :class:`Departamento` a mostrar"""
-		
-		departamento = model.Departamento.get(departamento)
-		affiliates = model.Affiliate.selectBy(departamento=departamento)
-		loans = list()
-		for affiliate in affiliates:
-			loans.extend(affiliate.loans)
-		
-		return dict(loans=loans, day=date.today())
-	
-	
-	@identity.require(identity.not_anonymous())
-	@expose(template='turboaffiliate.templates.affiliate.billing.loans')
-	@validate(validators=dict(school=validators.String()))
-	def loanSchool(self, school):
-		
-		"""Muestra los estados de cuenta préstamos de los afiliados de un
-		departamento
-		
-		:param school: El colegio a mostrar """
-		
-		affiliates = model.Affiliate.selectBy(school=school)
-		loans = list()
-		for affiliate in affiliates:
-			loans.extend(affiliate.loans)
-		
-		return dict(loans=loans, day=date.today())
+    @identity.require(identity.not_anonymous())
+    @expose(template='turboaffiliate.templates.affiliate.billing.index')
+    def index(self):
+
+        return dict()
+
+    @identity.require(identity.not_anonymous())
+    @expose(template='turboaffiliate.templates.affiliate.billing.status')
+    @validate(validators=dict(departamento=validators.Int()))
+    def state(self, departamento):
+
+        """Muestra los estados de cuenta de aportaciones de los afiliados de un
+        Departamento
+
+        :param departamento: El identificador del :class:`Departamento` a
+        mostrar
+        """
+
+        departamento = model.Departamento.get(departamento)
+        affiliates = model.Affiliate.selectBy(departamento=departamento)
+
+        return dict(affiliates=affiliates, day=date.today())
+
+    @identity.require(identity.not_anonymous())
+    @expose(template='turboaffiliate.templates.affiliate.billing.status')
+    @validate(validators=dict(payment=validators.String()))
+    def payment(self, payment):
+
+        affiliates = model.Affiliate.selectBy(payment=payment)
+        affiliates = (a for a in affiliates if a.joined != None)
+
+        return dict(affiliates=affiliates, day=date.today())
+
+    @identity.require(identity.not_anonymous())
+    @expose(template='turboaffiliate.templates.affiliate.billing.status')
+    @validate(validators=dict(school=validators.String()))
+    def school(self, school):
+
+        affiliates = model.Affiliate.selectBy(school=school)
+        affiliates = (a for a in affiliates if a.joined != None)
+
+        return dict(affiliates=affiliates, day=date.today())
+
+    @identity.require(identity.not_anonymous())
+    @expose(template='turboaffiliate.templates.affiliate.billing.status')
+    @validate(validators=dict(start=validators.Int(), end=validators.Int))
+    def rango(self, start, end):
+
+        affiliates = model.Affiliate.select(AND(model.Affiliate.q.id >= start,
+                                                model.Affiliate.q.id <= end))
+        affiliates = (a for a in affiliates if a.joined != None)
+
+        return dict(affiliates=affiliates, day=date.today())
+
+    @identity.require(identity.not_anonymous())
+    @expose(template='turboaffiliate.templates.affiliate.billing.loans')
+    @validate(validators=dict(departamento=validators.Int()))
+    def loanState(self, departamento):
+
+        """Muestra los estados de cuenta préstamos de los afiliados de un
+        departamento
+
+        :param departamento: El identificador del :class:`Departamento` a
+        mostrar"""
+
+        departamento = model.Departamento.get(departamento)
+        affiliates = model.Affiliate.selectBy(departamento=departamento)
+        loans = list()
+        for affiliate in affiliates:
+            loans.extend(affiliate.loans)
+
+        return dict(loans=loans, day=date.today())
+
+    @identity.require(identity.not_anonymous())
+    @expose(template='turboaffiliate.templates.affiliate.billing.loans')
+    @validate(validators=dict(school=validators.String()))
+    def loanSchool(self, school):
+
+        """Muestra los estados de cuenta préstamos de los afiliados de un
+        departamento
+
+        :param school: El colegio a mostrar """
+
+        affiliates = model.Affiliate.selectBy(school=school)
+        loans = list()
+        for affiliate in affiliates:
+            loans.extend(affiliate.loans)
+
+        return dict(loans=loans, day=date.today())
